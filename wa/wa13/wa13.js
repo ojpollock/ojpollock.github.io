00f7dd48a50ef5d76a44d2abe526c95f4fc79195
ojpollock <olpo6814@colorado.edu>
2023-04-21 15:01:00 -0600

    wa13

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -1,74 +1,65 @@
-
-
-function check() {
-    console.log('test');
-}
-
-function submit() {
-    alert(output.textContent);
-}
-
-function reset() {
-    outputInt = 0;
-    output.textContent = outputInt;
-}
-
-function minus() {
-    if (outputInt > 0) {
-    outputInt -=1;
-    output.textContent = outputInt; }
+function nextStep(currentStep, nextStep) {
+    // Get form values
+    var res1 = document.getElementById("res1").value;
+    var res2 = document.getElementById("res2").value;
     
+    // Validate form fields
+    if (currentStep === "step1" && res1 != 'yes please') {
+        alert('be polite and say _ _ _ please.');
+    } else if (currentStep === "step2" && (res2 != 'plus one please' && res2 != 'minus one please')) {
+        alert('only ONE at a time buddy, and again, say please!');
+    } else {
+        // Hide current step and show next step
+        document.getElementById(currentStep).style.display = 'none';
+        document.getElementById(nextStep).style.display = 'block';
+    }
+}
+    // const responses = ["plus one please", "minus one please"}
+
+
+function submitForm() {
+    // Get form values
+    var res1 = document.getElementById("res1").value;
+    var res2 = document.getElementById("res2").value;
+    
+    // Validate form fields
+    if (res1 === '' || res2 === '') {
+    alert('Please fill in all fields');
+    } else {
+        if (res2 === 'plus one please') {
+        increase();
+        } else  {
+            if (res2 === 'minus one please')
+        decrease();
+        } 
+                // Send form data to server
+        document.getElementById("res1").value = '';
+        document.getElementById("res2").value = '';
+        
+        // Show first step and hide others
+        document.getElementById("step1").style.display = 'block';
+        document.getElementById("step2").style.display = 'none';
+        document.getElementById("step3").style.display = 'none';
+    }
+
 }
 
-function plus() {
+function increase() {
     if (outputInt < 100) {
     outputInt +=1;
     output.textContent = outputInt;
     }
 }
 
-function random() {
-    outputInt = randomNumber(0, 100);
-    output.textContent = outputInt;
+// Function to decrease the number by two
+function decrease() {
+        if (outputInt > 0) {
+        outputInt -=1;
+        output.textContent = outputInt; }
+
 }
-
-function randomNumber(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
-  }
-
 
 
 const output = document.querySelector('.output');
 let outputInt = parseInt(output.textContent);
-console.log(outputInt);
 
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
-}
-*/
-
-
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
-}
